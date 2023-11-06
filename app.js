6b3694aa424c81250c9c3b61728e4bf41b8bd455
escasperlibero <149910514+escasperlibero@users.noreply.github.com>
2023-11-05 22:58:02 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  // Modifique o retorno para exibir seu nome
+  const meuNome = 'Seu Nome';
+
+  return (
+    <div>
+      <h1>Meu Nome é {meuNome}</h1>
+    </div>
+  );
+}
+
+export default App;
